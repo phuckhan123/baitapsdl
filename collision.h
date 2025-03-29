23e8c6083eb54ec67d9ac8a645770df95eb92cb0
BinhPhuc <pham.phuc.binh.271106@gmail.com>
2025-03-30 01:53:21 +0700

    new ver

--- collision.h
+++
@@ -1,9 +0,0 @@
-#ifndef COLLISION_H
-#define COLLISION_H
-
-#include <SDL.h>
-
-// Hàm kiểm tra va chạm hình tròn và hình chữ nhật
-bool checkCircleRectCollision(int cx, int cy, int r, SDL_Rect rect);
-
-#endif
